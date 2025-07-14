3fe3cd7cbd4718beadd37ee05a2a4843778a6836
Yevhenii POIASKOV <poyaskovyevgeniy@gmail.com>
2025-07-14 22:48:08 +0300

    Update

--- Task-4/tests/cd-tests.sh
+++ Task-4/tests/cd-tests.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+g++ algorithms-KGood.cpp -o KGood.exe
 
 for ((i=1; i<=3; i++)); do
     test_input=$(head -n 2 "test-${i}.txt")
     expected_output=$(tail -n 1 "test-${i}.txt")
-    actual_output=$(echo -e "$test_input" | ../KGood.exe)
+    actual_output=$(echo -e "$test_input" | ./KGood.exe)
     
     if [ "$actual_output" = "$expected_output" ]; then
         echo
