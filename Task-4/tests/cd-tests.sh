202e1f3da766273c8e3cb3144b808168934d3a1f
Yevhenii POIASKOV <poyaskovyevgeniy@gmail.com>
2025-07-14 22:38:19 +0300

    Add file

---
+++ Task-4/tests/cd-tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+for ((i=1; i<=3; i++)); do
+    test_input=$(head -n 2 "test-${i}.txt")
+    expected_output=$(tail -n 1 "test-${i}.txt")
+    actual_output=$(echo -e "$test_input" | ../KGood.exe)
+    
+    if [ "$actual_output" = "$expected_output" ]; then
+        echo
+        echo "Test-${i}: success"
+        echo "Input:"
+        echo "$test_input"
+        echo "Output: $actual_output"
+        
+    else 
+        echo
+        echo "Test-${i}: fail"
+        echo "Output: $actual_output"
+        echo "Expected output: $expected_output"
+        break
+    fi
+done
